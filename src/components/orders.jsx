63a7f2f7fb7a5c6c35a85e47888f2bda0bd73d85
Fadi-Maher <cleverfady@gmail.com>
2024-09-10 01:21:04 +0300

    edit order page

--- src/components/orders.jsx
+++ src/components/orders.jsx
@@ -1,10 +1,10 @@
  
-function Orders() {
+function allorders() {
   return (
-    <div className='pt-10 pb-10 mt-60 mb-60 text-white text-4xl bg-green-500 text-center'>
+    <div className='pt-10 pb-10 mt-16 mb-60 text-white text-4xl bg-green-500 text-center'>
       Your order is done !
     </div>
   )
 }
 
-export default Orders
+export default allorders
